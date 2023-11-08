f9b977533edcf80275d6dd9aa08da3b4042b549e
unknown <Hanu-soni>
2023-11-09 02:23:21 +0530

    node version

--- config/dbcon.js
+++ config/dbcon.js
@@ -12,7 +12,7 @@ const dbURL=process.env.Mongodb;
 mongoose.connect(dbURL, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
-   bufferCommands: false, // Set this to true to enable buffering
+   bufferCommands: true, // Set this to true to enable buffering
   bufferMaxEntries: 0,   // Set the maximum number of entries in the buffer
   connectTimeoutMS: 30000, // Set the connection timeout
  }).then(()=>{
